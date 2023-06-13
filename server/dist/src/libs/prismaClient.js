7d8cf2d0bc4bdcb3bcb083f743dac719716925ca
kortoaus <kortoaus@gmail.com>
2023-06-13 19:00:59 +1000

    remove dist

--- server/dist/src/libs/prismaClient.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = require("@prisma/client");
-var client = global.client || new client_1.PrismaClient({ log: [] });
-if (process.env.NODE_ENV === "development")
-    global.client = client;
-exports.default = client;
